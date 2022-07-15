7524646e8a8e7b7b35607cb6d27cfd6524c5ea1f
clipper-dev <96745726+clipper-dev@users.noreply.github.com>
2022-07-15 19:38:00 +0200

    Web commit 1

--- src/components/pages/Book.js
+++ src/components/pages/Book.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import './Book.css'
 
 function Book() {
   return (
-    <div>Book</div>
+    <>
+      <img className='image-background' src='/book-banner.png' />
+      <div className='main-content'>
+        <h1 className='text--centered'>Tools of The Smart Work: </h1>
+        <h2 className='text--centered'>Tools and techniques to do better and achieve more in less time</h2>
+
+        <h1 className='text--centered'>Comming soon.</h1>
+      </div>
+    </>
   )
 }
 
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import './Home.css'
 
 function Home() {
   return (
     <>
-    <div className='main-content'>
+    <div className='main-content text--centered'>
         <h1>The Only Productivity Blog You Need</h1>
 
     </div>
